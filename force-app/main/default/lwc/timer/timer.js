e6c3d9e155b2310c71a4cfd73adaa01fcd91b392
SurajRathod <surajrathod999@gmail.com>
2020-10-19 15:34:12 +0530

    added functionality for Timer Components

--- force-app/main/default/lwc/timer/timer.js
+++ force-app/main/default/lwc/timer/timer.js
@@ -1,9 +1,77 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api ,track } from 'lwc';
 
 export default class Timer extends LightningElement {
-    isPlaying=false;
+    @api focusTimeValue
+    @track Seconds=0;
+    @track Minute=0;
+    playing = false;
+    countdown = null;
+    timerId = null;
 
-    changeevent(){
-        this.isPlaying=!this.isPlaying;
+    connectedCallback(){
+        this.Minute=this.focusTimeValue;
     }
+    
+  counter () {
+    if (!this.playing) {
+      clearInterval(this.timerId)
+      return
+    }
+    // this will work for only initial
+    // when the countdown is null
+    // this is for the functionality of pauseTimer
+    if (this.countdown == null) {
+      this.countdown = this.focusTimeValue * 60 * 1000
+    }
+    this.timerId = setInterval(() => {
+      if (this.playing === true) {
+        this.countdown -= 1000
+        const min = Math.floor(this.countdown / (60 * 1000))
+        const sec = Math.floor((this.countdown - (min * 60 * 1000)) / 1000)
+
+        // countdown gows below 0
+        // stop the timer
+        // send message to the main process
+        if (this.countdown <= 0) {
+          this.stopTimer()
+          
+        } else {
+          this._renderMinuteSecond(min, sec)
+        }
+      }
+    }, 1000)
+  }
+
+  startTimer () {
+    // change the playing to true call the counter
+    this.playing = true
+    this.counter()
+  }
+
+  pauseTimer () {
+    // pause the timer
+    this.playing = false
+
+    clearInterval(this.timerId)
+  }
+
+  @api stopTimer () {
+    // stop the timer be clearing the timerid
+    // setting mull to countdown
+    this.countdown = null
+    clearInterval(this.timerId)
+    this.timerId = null
+    this.playing = false
+    this._renderMinuteSecond(this.focusTimeValue,0);
+  }
+
+  isPlaying () {
+    return this.playing
+  }
+
+  _renderMinuteSecond (min, sec) {
+    this.Minute = min
+    this.Seconds= sec
+  }
+    
 }
